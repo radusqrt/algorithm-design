f911921d48f984b1fc5101194614a4637cf3c106
Antonio <antoniomacovei@yahoo.com>
2018-10-01 12:31:42 +0300

    Fixed indentation v2

--- CPP/data-structures/Hashtable.h
+++ CPP/data-structures/Hashtable.h
@@ -10,61 +10,61 @@ template <typename Tkey, typename Tvalue> struct elem_info {
 };
 
 template <typename Tkey, typename Tvalue> class Hashtable {
-	private:
-		std::list<struct elem_info<Tkey, Tvalue> > * H;  // pointer to buckets
-		int HMAX;  // number of buckets
-		unsigned int (*hash)(Tkey);  // pointer to hash function
-		unsigned int size;  // number of elements in hashtable
+private:
+	std::list<struct elem_info<Tkey, Tvalue> > * H;  // pointer to buckets
+	int HMAX;  // number of buckets
+	unsigned int (*hash)(Tkey);  // pointer to hash function
+	unsigned int size;  // number of elements in hashtable
 
-	public:
-		Hashtable(int hmax, unsigned int (*h)(Tkey)) {
-			HMAX = hmax;
-			H = new std::list<struct elem_info<Tkey, Tvalue> >[hmax];
-			hash = h;
-		}
+public:
+	Hashtable(int hmax, unsigned int (*h)(Tkey)) {
+		HMAX = hmax;
+		H = new std::list<struct elem_info<Tkey, Tvalue> >[hmax];
+		hash = h;
+	}
 
-		~Hashtable() {
-			delete[] H;
-		}
+	~Hashtable() {
+		delete[] H;
+	}
 
-		// Adds an element to the hashtable
-		void put(Tkey key, Tvalue value) {
-			unsigned int index = hash(key) % HMAX;
-			bool found = false;
-			for(auto it : H[index]) {
-				if(it.key == key) {
-					it.value = value;
-					found = true;
-				}
-			}
-			if(!found) {
-				elem_info<Tkey, Tvalue> new_elem;
-				new_elem.key = key;
-				new_elem.value = value;
-				H[index].push_back(new_elem);
+	// Adds an element to the hashtable
+	void put(Tkey key, Tvalue value) {
+		unsigned int index = hash(key) % HMAX;
+		bool found = false;
+		for(auto it : H[index]) {
+			if(it.key == key) {
+				it.value = value;
+				found = true;
 			}
 		}
+		if(!found) {
+			elem_info<Tkey, Tvalue> new_elem;
+			new_elem.key = key;
+			new_elem.value = value;
+			H[index].push_back(new_elem);
+		}
+	}
 
-		// Gets an element from the hashtable
-		Tvalue get(Tkey key) {
-			unsigned int index = hash(key) % HMAX;
-			for(auto it : H[index]) {
-				if(it.key == key) {
-					return it.value;
-				}
+	// Gets an element from the hashtable
+	Tvalue get(Tkey key) {
+		unsigned int index = hash(key) % HMAX;
+		for(auto it : H[index]) {
+			if(it.key == key) {
+				return it.value;
 			}
-			return Tvalue();
 		}
+		return Tvalue();
+	}
 
-		// Gets the hashtable size.
-		int get_size() {
-			return size;
-		}
+	// Gets the hashtable size.
+	int get_size() {
+		return size;
+	}
 
-		// Gets the hastable capacity.
-		int get_capacity() {
-			return HMAX;
-		}
+	// Gets the hastable capacity.
+	int get_capacity() {
+		return HMAX;
+	}
 };
 
 #endif  // HASHTABLE_H__
