87bf95b15e193210820efcb8f0b1a341a0850c1f
Antonio <antoniomacovei@yahoo.com>
2018-10-01 12:28:33 +0300

    Fixed indentation and comments

--- CPP/data-structures/DoublyLinkedList.h
+++ CPP/data-structures/DoublyLinkedList.h
@@ -1,8 +1,3 @@
-/*
-	DoublyLinkedList.h
-	Copyright Antonio Macovei
-*/
-
 #ifndef DOUBLYLINKEDLIST_H_
 #define DOUBLYLINKEDLIST_H_
 
@@ -21,149 +16,126 @@ struct Node {
 
 template <typename T>
 class DoublyLinkedList {
-	private:
-		Node<T> *head;
-		Node<T> *tail;
-		int numElements;
+private:
+	Node<T> *head;
+	Node<T> *tail;
+	int numElements;
 
-		// Returns the pos'th node in the list.
-		Node<T> *goToPos(int pos) {
-		    int count = 0;
-		    Node<T> *it = head;
+	// Returns the pos'th node in the list
+	Node<T> *goToPos(int pos) {
+		int count = 0;
+		Node<T> *it = head;
 
-		    while (count != pos) {
-		        it = it->next;
-		        count++;
-		    }
-		    return it;
+		while (count != pos) {
+			it = it->next;
+			count++;
 		}
+		return it;
+	}
 
-	public:
-		// Constructor
-		DoublyLinkedList() {
-			head = nullptr;
+public:
+	DoublyLinkedList() {
+		head = nullptr;
+		tail = nullptr;
+		numElements = 0;
+	}
+
+	~DoublyLinkedList() {
+		while (head != nullptr) {
+			Node<T> *temp = head;
+			head = head->next;
+			delete temp;
+		}
+	}
+
+	// Adds a new node at the beginning of the list
+	void add_first(T data) {
+		if (head == nullptr) {
+			head = new Node<T>(data);
+			head->next = nullptr;
+			head->prev = nullptr;
+			tail = head;
+		} else {
+			Node<T> *new_node;
+			new_node = new Node<T>(data);
+			new_node->next = head;
+			new_node->prev = nullptr;
+			head->prev = new_node;
+			head = new_node;
+		}
+		numElements++;
+	}
+
+	// Adds a new node at the end of the list
+	void add_last(T data) {
+		if (head == nullptr) {
+			head = new Node<T>(data);
+			head->next = nullptr;
+			head->prev = nullptr;
+			tail = head;
+		} else {
+			Node<T> *new_node;
+			new_node = new Node<T>(data);
+			new_node->next = nullptr;
+			new_node->prev = tail;
+			tail->next = new_node;
+			tail = new_node;
+		}
+		numElements++;
+	}
+
+	// Removes the last node of the list
+	void remove_last() {
+		if (numElements == 0) return;
+		if (numElements == 1) {
+			delete tail;
 			tail = nullptr;
+			head = nullptr;
 			numElements = 0;
+		} else {
+			Node<T> *new_tail = tail->prev;
+			tail->prev->next = nullptr;
+			delete tail;
+			tail = new_tail;
+			numElements--;
 		}
+	}
 
-		// Destructor
-		~DoublyLinkedList() {
-			while (head != nullptr) {
-				Node<T> *temp = head;
-				head = head->next;
-				delete temp;
-			}
+	// Removes the first node of the list
+	void remove_first() {
+		if (numElements == 0) return;
+		if (numElements == 1) {
+			delete head;
+			tail = nullptr;
+			head = nullptr;
+			numElements = 0;
+		} else {
+			Node<T> *new_head = head->next;
+			head->next->prev = nullptr;
+			delete head;
+			head = new_head;
+			numElements--;
 		}
+	}
 
-		/**
-		 * Adds a new node at the beginning of the list.
-		 *
-		 * @param data Data to be added at the end of the list.
-		 */
-		void add_first(T data) {
-			if (head == nullptr) {
-				head = new Node<T>(data);
-				head->next = nullptr;
-				head->prev = nullptr;
-				tail = head;
-			} else {
-				Node<T> *new_node;
-				new_node = new Node<T>(data);
-				new_node->next = head;
-				new_node->prev = nullptr;
-				head->prev = new_node;
-				head = new_node;
-			}
-			numElements++;
-		}
+	// Check if the list contains any elements
+	bool is_empty() {
+		if (numElements == 0)
+			return true;
+		return false;
+	}
 
-		/**
-		 * Adds a new node at the end of the list.
-		 *
-		 * @param data Data to be added at the end of the list.
-		 */
-		void add_last(T data) {
-			if (head == nullptr) {
-				head = new Node<T>(data);
-				head->next = nullptr;
-				head->prev = nullptr;
-				tail = head;
-			} else {
-				Node<T> *new_node;
-				new_node = new Node<T>(data);
-				new_node->next = nullptr;
-				new_node->prev = tail;
-				tail->next = new_node;
-				tail = new_node;
-			}
-			numElements++;
-		}
+	// Get the number of nodes in the list
+	int size() {
+		return numElements;
+	}
 
-		/**
-		* Removes the last node of the list.
-		*/
-		void remove_last() {
-			if (numElements == 0) return;
-			if (numElements == 1) {
-				delete tail;
-				tail = nullptr;
-				head = nullptr;
-				numElements = 0;
-			} else {
-				Node<T> *new_tail = tail->prev;
-				tail->prev->next = nullptr;
-				delete tail;
-				tail = new_tail;
-				numElements--;
-			}
-		}
+	Node<T> *get_head() {
+		return head;
+	}
 
-		/**
-		 * Removes the first node of the list.
-		 */
-		void remove_first() {
-			if (numElements == 0) return;
-			if (numElements == 1) {
-				delete head;
-				tail = nullptr;
-				head = nullptr;
-				numElements = 0;
-			} else {
-				Node<T> *new_head = head->next;
-				head->next->prev = nullptr;
-				delete head;
-				head = new_head;
-				numElements--;
-			}
-		}
-
-		/**
-		 * Check if the list contains any elements.
-		 *
-		 * @return True if the list contains no elements, False otherwise.
-		 */
-		bool is_empty() {
-			if (numElements == 0)
-				return true;
-			return false;
-		}
-
-		/**
-		 * Get the number of nodes in the list.
-		 *
-		 * @return The number of nodes stored in the list.
-		 */
-		int size() {
-			return numElements;
-		}
-
-		// Getters & Setters
-		Node<T> *get_head() {
-			return head;
-		}
-
-		Node<T> *get_tail() {
-			return tail;
-		}
+	Node<T> *get_tail() {
+		return tail;
+	}
 };
 #endif  // DOUBLYLINKEDLIST_H_
